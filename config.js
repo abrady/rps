dc4ee6c90003de6f5bceaa10fd11703e6bfc966f
Aaron Brady <ab@fb.com>
2011-11-07 19:23:04 -0800

    update config name

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.host       = "superfbrps.herokuapp.com";
-exports.app_name   = "Rock-Paper-Scissors-Dev";
+exports.app_name   = "Rock-Paper-Scissors";
 exports.app_id     = '224658110883993';
 exports.app_secret = 'd37dfe315494b6959a59a8571c7a8f51';
 exports.fb_domain_modifier = '';// abrady.dev2899.'; // set this to dev sandbox, e.g. foo.dev1234. (don't forget trailing '.')
