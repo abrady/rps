cb63ec97a5488c88965a2909c43728327e7957ee
Aaron Brady <ab@fb.com>
2011-11-07 18:57:00 -0800

    proper config

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 exports.host       = "superfbrps.herokuapp.com";
 exports.app_name   = "Rock-Paper-Scissors-Dev";
-exports.app_id     = '126449567461197';
-exports.app_secret = '5c70f79651ff0a3374855670fa71daca';
+exports.app_id     = '224255097586328';
+exports.app_secret = '6dc1ff7ad4f0f4dcbeb0f21b1c8bbb57';
+exports.fb_domain_modifier = '';// abrady.dev2899.'; // set this to dev sandbox, e.g. foo.dev1234. (don't forget trailing '.')
+// NA for heroku:
 exports.http_port  = 80; //8081;
 exports.https_port = 8082;
-exports.fb_domain_modifier = '';// abrady.dev2899.'; // set this to dev sandbox, e.g. foo.dev1234. (don't forget trailing '.')
+
